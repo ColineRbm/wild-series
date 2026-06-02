26ca41d56ebbef1332a92254b631b7100d89da39
Coline Rabemihoatra <coline.rbm@gmail.com>
2026-06-02 09:25:22 +0200

    init: wild-series project

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,19 @@
 import "dotenv/config";
 
 // Check database connection
-// Note: This is optional and can be removed if the database connection
-// is not required when starting the application
 import "../database/checkConnection";
 
 // Import the Express application from ./app
 import app from "./app";
+
+import type { RequestHandler } from "express";
+
+// Route GET /
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+app.get("/", sayWelcome);
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
