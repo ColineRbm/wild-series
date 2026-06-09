956849ce637bde09e1ef04f077501bce3c157040
Coline Rabemihoatra <coline.rbm@gmail.com>
2026-06-09 15:00:39 +0200

    feat: add program repository and connect browse to database

--- server/src/modules/program/programActions.ts
+++ server/src/modules/program/programActions.ts
@@ -1,4 +1,6 @@
-// Some data to make the trick
+import programRepository from "./programRepository";
+
+import type { RequestHandler } from "express";
 
 const programs = [
   {
@@ -23,34 +25,19 @@ const programs = [
   },
 ];
 
-// Declare the action
-
-import type { RequestHandler } from "express";
-
-const browse: RequestHandler = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = programs.filter((program) =>
-      program.synopsis.includes(req.query.q as string),
-    );
-
-    res.json(filteredPrograms);
-  } else {
-    res.json(programs);
-  }
+const browse: RequestHandler = async (req, res) => {
+  const programsFromDB = await programRepository.readAll();
+  res.json(programsFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
   const parsedId = Number.parseInt(req.params.id);
-
   const program = programs.find((p) => p.id === parsedId);
-
   if (program != null) {
     res.json(program);
   } else {
     res.sendStatus(404);
   }
 };
-
-// Export it to import it somewhere else
 
 export default { browse, read };
