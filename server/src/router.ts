808c42f62766c28c949f99dc5b10a91118da29dc
Coline Rabemihoatra <coline.rbm@gmail.com>
2026-06-09 11:00:58 +0200

    feat: add category browse and read routes

--- server/src/router.ts
+++ server/src/router.ts
@@ -13,7 +13,19 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+// Define program-related routes
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
 /* ************************************************************************* */
+
+// Define category-related routes
+import categoryActions from "./modules/category/categoryActions";
+
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 // Declaration of a "Welcome" route
 
@@ -22,8 +34,5 @@ import sayActions from "./modules/say/sayActions";
 router.get("/", sayActions.sayWelcome);
 
 /* ************************************************************************* */
-
-import programActions from "./modules/program/programActions";
-router.get("/api/programs", programActions.browse);
 
 export default router;
